b6e29a817a26a907b0bee25924642885410c1ef5
Greg Lee <gml2136@gmail.com>
2017-05-03 21:41:52 -0700

    initial copy of eat-da-burger

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require('method-override');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//overriding post requests with ?_method = put
+app.use(methodOverride("_method"));
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controllers.js");
+
+
+app.use("/", routes);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
